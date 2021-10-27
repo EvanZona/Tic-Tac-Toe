24fce315e8d7c148b3b48d38d215d60b2772627c
Evan Zona <evanbzona@gmail.com>
2021-10-27 10:49:49 -0500

    Initial commit

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Tic Tac Toe")
+                .fontWeight(.bold)
+                .font(.title)
+                .padding()
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
